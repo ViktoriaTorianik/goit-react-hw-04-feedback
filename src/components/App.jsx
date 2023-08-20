5320498533e94a145ab551d765b564ac47beca4f
ViktoriaTorianik <122830538+ViktoriaTorianik@users.noreply.github.com>
2023-08-20 12:56:55 +0300

    Хук useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,55 @@
-export const App = () => {
+import { useState } from 'react';
+import React from 'react';
+
+import Feedback from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/statistics';
+import Wrapper from './Wrapper/wrapper';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
+
+export default function FormFeedback() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const feedback = e => {
+    switch (e) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
+  };
+  const countTotalFeedback = () => good + neutral + bad;
+  const countPositiveFeedbackPercentage = () =>
+    Math.round((good / countTotalFeedback()) * 100);
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Wrapper>
+      <Section title="Please leave feedback">
+        <Feedback
+          options={Object.keys({ good, neutral, bad })}
+          onFeedback={feedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            netural={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </Wrapper>
   );
-};
+}
